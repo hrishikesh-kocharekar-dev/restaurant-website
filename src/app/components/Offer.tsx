313c2f98c7974db6c3b615a1124df148ac15721b
Hrishikesh kocharekar <127331666+hrishikesh-kocharekar-dev@users.noreply.github.com>
2023-08-26 01:25:53 +0530

    Initial commit

--- src/app/components/Offer.tsx
+++ src/app/components/Offer.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Image from "next/image";
 // import CountDown from "./CountDown";
-// import CountDown from "./CountDown";
+
 
 export default function Offer() {
   return (
