fd7d8d0d1dda2638cdaea6600b1fb7c93e36e2d3
Hrishikesh kocharekar <127331666+hrishikesh-kocharekar-dev@users.noreply.github.com>
2023-09-09 23:51:03 +0530

    login page added

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
 
-export default function LoginPage() {
+const LoginPage = () => {
   return (
-    <div>LoginPage</div>
-  )
-}
+    <div className="p-4 h-[calc(100vh-6rem)] flex items-center justify-center md:h-[calc(100vh-9rem)] ">
+      <div className=" h-full rounded-md flex flex-col md:flex-row md:h-[70%]  lg:w-[60%]  shadow-xl md:w-full 2xl:w-1/2">
+        <div className="relative md:h-full h-1/3  md:w-1/2  w-full ">
+          <Image src="/slide3.jpg" alt="" fill className="object-cover" />
+        </div>
+
+      
+      </div>
+    </div>
+  );
+};
+
+export default LoginPage;
